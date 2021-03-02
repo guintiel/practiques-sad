0ecfffb688a2a986bb6392cb5b16377c3ab674fc
Xavi <xguinotv@gmail.com>
2021-03-02 16:27:17 +0100

    start

--- practica-1/EditableBufferedReader.java
+++ practica-1/EditableBufferedReader.java
@@ -6,5 +6,20 @@ import java.io.BufferedReader;
 
 public class EditableBufferedReader extends BufferedReader {
     
+    public void setRaw(){
+        //setRaw method passa la consola de mode cooked a mode raw.
+    }
+
+    public void unsetRaw(){
+        //unsetRaw method passa la consola de mode raw a mode cooked.
+    }
+
+    public int read(){
+        //read method llegeix el següent caràcter o la següent tecla de cursor.
+    }
+
+    public int readLine(){
+        //readLine method llegeix la línia amb possibilitat d’editar-la.
+    }
 
 }
