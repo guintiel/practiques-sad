972e23ff7031960bd5d7aa58926b4fde7e77625f
Xavi <xavierguinot1@gmail.com>
2021-03-04 17:41:57 +0100

    1st try implementation line

--- practica-1/src/Line.java
+++ practica-1/src/Line.java
@@ -1,5 +1,30 @@
 package src;
 
+import java.util.*;
+
 public class Line {
     
+    private ArrayList<Integer> list;
+    private Boolean editable;
+    private int lenght;
+
+    public Line(){
+        this.list = new ArrayList<Integer>();
+        this.editable = true;
+        this.lenght = 0;
+    }
+
+    public void addChar(int c){
+
+    }    
+    
+    /* public void removeChar(int c){
+            // a read
+    } */
+
+    public String toString(){
+
+    }
+
+
 }
