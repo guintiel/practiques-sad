d58c7b56157488e1dd931981b22cdcbaf460edd0
monti98 <jordimontiel11@gmail.com>
2021-03-17 17:33:11 +0100

    Canvis Jordi

--- practica-1/src/EditableBufferedReader.java
+++ practica-1/src/EditableBufferedReader.java
@@ -1,19 +1,41 @@
 package src;
 
 import java.io.*;
+import java.util.ArrayList;
 //import java.util.ArrayList;
 //import java.lang.Integer;
+import java.util.List;
 
 public class EditableBufferedReader extends BufferedReader {
 
     private Line linia;
+    public static final int RIGHT_ARROW = 67;
+    public static final int LEFT_ARROW = 68;
+
+    public static final int HOME = 72;
+    public static final int END = 70;
+
+    public static final int INS = 50;
+
+    public static final int DEL = 51;
+    public static final int BKSP = 127;
+
+    public static final int CTRL_C = 3;
 
     public EditableBufferedReader(Reader in) {
         super(in);
+        linia = new Line(getCols());
     }
 
     public void setRaw() {
-        ProcessBuilder p = new ProcessBuilder("stty -echo raw </dev/tty");
+        // ArrayList<String> com = new ArrayList<String>();
+        // com.add(e)
+        final List<String> command = new ArrayList<String>();
+        command.add("/bin/sh");
+        command.add("-c");
+        // command.add("");
+        command.add("stty -echo raw </dev/tty");
+        ProcessBuilder p = new ProcessBuilder(command);
         try {
             p.start();
         } catch (IOException e) {
@@ -23,7 +45,12 @@ public class EditableBufferedReader extends BufferedReader {
     }
 
     public void unsetRaw() {
-        ProcessBuilder p = new ProcessBuilder("stty -echo cooked </dev/tty");
+        final List<String> command = new ArrayList<String>();
+        command.add("/bin/sh");
+        command.add("-c");
+        // command.add("");
+        command.add("stty -echo -raw </dev/tty");
+        ProcessBuilder p = new ProcessBuilder(command);
         try {
             p.start();
         } catch (IOException e) {
@@ -45,28 +72,47 @@ public class EditableBufferedReader extends BufferedReader {
 
     public String readLine() {
         // readLine method llegeix la línia amb possibilitat d’editar-la.
-        int maxcols = 200;
         this.setRaw();
         int charac = this.read();
         // int maxCols = parseInt(this.getCols());
-        while (charac != 3) {
-            charac = this.read();
+        while (charac != CTRL_C) {
             switch (charac) {
+            case RIGHT_ARROW:
+                this.linia.setCursorPos(this.linia.getCursorPos() + 1);
+                break;
+            case LEFT_ARROW:
+                this.linia.setCursorPos(this.linia.getCursorPos() - 1);
+                break;
+            case HOME:
+                this.linia.setCursorPos(0);
+            case END:
+                this.linia.setCursorPos(this.linia.getLength());
+            case INS:
+
+            case DEL:
 
             default:
                 this.linia.addCar((char) charac);
+                System.out.print((char) charac);
+                break;
             }
+            charac = this.read();
         }
         this.unsetRaw();
         return this.linia.toString();
     }
 
-    public String getCols() throws IOException {
-        Process p = Runtime.getRuntime().exec("tput cols");
-        InputStream input = p.getInputStream();
-        BufferedReader r = new BufferedReader(new InputStreamReader(input));
-        String num = r.readLine();
-        return num;
+    public int getCols() {
+        try {
+            Process p = Runtime.getRuntime().exec("tput cols");
+            InputStream input = p.getInputStream();
+            BufferedReader r = new BufferedReader(new InputStreamReader(input));
+            int col = Integer.parseInt(r.readLine());
+            return col;
+        } catch (IOException e) {
+            return 0;
+        }
+
     }
 
 }
--- practica-1/src/Line.java
+++ practica-1/src/Line.java
@@ -4,22 +4,43 @@ import java.util.*;
 
 public class Line {
 
-    private ArrayList<Integer> list;
+    private ArrayList<Integer> characters;
     // private Boolean editable;
     private int length;
+    private int cursorPos;
 
     public Line(int len) {
-        this.list = new ArrayList<Integer>();
+        this.characters = new ArrayList<Integer>();
         // this.editable = true;
         this.length = len;
+        this.cursorPos = 0;
     }
 
     public void addCar(int car) {
-        list.add(car);
+        if (characters.size() < this.length) {
+            characters.add(car);
+        }
     }
 
-    // public String toString(){
+    public void setCursorPos(int cursorPos) {
+        this.cursorPos = cursorPos;
+    }
 
-    // }
+    public int getCursorPos() {
+        return this.cursorPos;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    @Override
+    public String toString() {
+        String cad = "";
+        for (int i = 0; i < characters.size(); i++) {
+            cad = cad + (char) characters.get(i).byteValue();
+        }
+        return cad;
+    }
 
 }
